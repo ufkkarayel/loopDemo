dc133e8b9fe1dc174aa3aa58298c4ac460bca2d1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2022-09-20 00:12:31 +0300

    while loop added.

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //For döngüsü
+
+        for (int i=1;i<=10;i+=2){
+            System.out.println(i);
+
+        }
+        System.out.println("For döngüsü bitti");
+
+        //while döngüsü
+        int i=1;
+
+        while (i<10){
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("While döngüsü bitti");
+    }
+}
